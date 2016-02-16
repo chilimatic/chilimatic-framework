6f64f35ba566ac03f99126015f752437620ed0df
Jakob Oberhummer <office@chilimatic.com>
2016-02-16 20:50:53 +0100

    - new filter options

--- lib/database/sql/orm/EntityObjectStorage.php
+++ lib/database/sql/orm/EntityObjectStorage.php
@@ -1,28 +1,67 @@
 <?php
-/**
- *
- * @author j
- * Date: 12/31/14
- * Time: 12:04 AM
- *
- * File: entityobjectstorage.class.php
- */
-
 namespace chilimatic\lib\database\sql\orm;
 
 class EntityObjectStorage extends \SplObjectStorage implements \JsonSerializable
 {
+    /**
+     * @var \ReflectionClass
+     */
+    private $reflection;
 
-    public function jsonSerialize()
+    /**
+     * keep them cached
+     *
+     * @var []
+     */
+    private $columMap;
+
+    /**
+     * @param string $columName
+     *
+     * @return array
+     */
+    public function getAsArray($columName = null)
     {
+        if ($columName) {
+            $this->rewind();
+            $obj = $this->current();
+            if ($obj) {
+                if (!$this->reflection) {
+                    $this->reflection = new \ReflectionClass($this->current());
+                }
+
+                if ($this->columMap === null) {
+                    $this->columMap = [];
+                    foreach ($this->reflection->getProperties() as $property) {
+                        $this->columMap[$property->getName()] = $property;
+                    }
+                }
+            }
+        }
+
+
         $this->rewind();
         $arr = [];
-
         while ($this->valid()) {
-            $arr[] = $this->current();
+            if ($columName && !empty($this->columMap[$columName])) {
+                $obj = $this->current();
+                /**
+                 * @var \ReflectionProperty $test
+                 */
+                $this->columMap[$columName]->setAccessible(true);
+                $arr[] = $this->columMap[$columName]->getValue($obj);
+            } else {
+                $arr[] = $this->current();
+            }
+
             $this->next();
         }
 
         return $arr;
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->getAsArray();
     }
 }
